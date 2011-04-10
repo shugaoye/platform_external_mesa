d39fa8735a1299f95ab3199bffa87dbe7f03eab7
Chia-I Wu <olvaffe@gmail.com>
2011-04-10 22:41:07 +0800

    android: fix build with honeycomb

--- src/gallium/winsys/sw/android/android_sw_winsys.cpp
+++ src/gallium/winsys/sw/android/android_sw_winsys.cpp
@@ -33,7 +33,6 @@
 #include "state_tracker/sw_winsys.h"
 
 #include <utils/Errors.h>
-#include <private/ui/sw_gralloc_handle.h>
 
 #include <hardware/gralloc.h>
 
@@ -106,13 +105,8 @@ android_displaytarget_unmap(struct sw_winsys *ws,
    struct android_sw_displaytarget *adt = android_sw_displaytarget(dt);
 
    if (adt->mapped) {
-      if (sw_gralloc_handle_t::validate(adt->handle) >= 0) {
-         adt->mapped = NULL;
-      }
-      else {
-         droid->grmod->unlock(droid->grmod, adt->handle);
-         adt->mapped = NULL;
-      }
+      droid->grmod->unlock(droid->grmod, adt->handle);
+      adt->mapped = NULL;
    }
 }
 
@@ -125,16 +119,9 @@ android_displaytarget_map(struct sw_winsys *ws,
    struct android_sw_displaytarget *adt = android_sw_displaytarget(dt);
 
    if (!adt->mapped) {
-      if (sw_gralloc_handle_t::validate(adt->handle) >= 0) {
-         const sw_gralloc_handle_t *swhandle =
-            reinterpret_cast<const sw_gralloc_handle_t *>(adt->handle);
-         adt->mapped = reinterpret_cast<void *>(swhandle->base);
-      }
-      else {
-         /* lock the buffer for CPU access */
-         droid->grmod->lock(droid->grmod, adt->handle,
-               adt->usage, 0, 0, adt->width, adt->height, &adt->mapped);
-      }
+      /* lock the buffer for CPU access */
+      droid->grmod->lock(droid->grmod, adt->handle,
+            adt->usage, 0, 0, adt->width, adt->height, &adt->mapped);
    }
 
    return adt->mapped;
